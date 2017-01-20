1c45193ae770124325a2ee50a6c547f2833cb7c0
Maria Júlia Berriel de Sousa <mjuliabs@gmail.com>
2017-01-20 16:39:20 -0200

    Update local_settings.py

--- local_settings.py
+++ local_settings.py
@@ -8,7 +8,7 @@ MY_CONSUMER_SECRET = 'rOm99m2SjqyX68DDsiVFvrsyCYRH12BEN241ZtEdogQLsK6KQn'
 MY_ACCESS_TOKEN_KEY = '822493022860574723-v6b6glgbHV0TkxXbgkdoRDrKp3L3e2b'
 MY_ACCESS_TOKEN_SECRET = 'MoyLvHPN1Z1uGrQuN7Ctiy4I1qQwd8Nrs463iaRBjrEO7'
 
-SOURCE_ACCOUNTS = ["neymarjr"] #A list of comma-separated, quote-enclosed Twitter handles of account that you'll generate tweets based on. It should look like ["account1", "account2"]. If you want just one account, no comma needed.
+SOURCE_ACCOUNTS = ["CamilaPitanga"] #A list of comma-separated, quote-enclosed Twitter handles of account that you'll generate tweets based on. It should look like ["account1", "account2"]. If you want just one account, no comma needed.
 ODDS = 1 #How often do you want this to run? 1/8 times?
 ORDER = 2 #how closely do you want this to hew to sensical? 1 is low and 3 is high.
 DEBUG = True #Set this to False to start Tweeting live
