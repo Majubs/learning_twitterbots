495d640df77d4cf1a320b3b2f7ee7a09c78fe008
Maria Júlia Berriel de Sousa <mjuliabs@gmail.com>
2017-01-20 16:27:46 -0200

    Update local_settings.py

--- local_settings.py
+++ local_settings.py
@@ -3,10 +3,10 @@ Local Settings for a heroku_ebooks account. #fill in the name of the account you
 '''
 
 #configuration
-MY_CONSUMER_KEY = 'AJPXDsJ4O4FYUletYZ0UzMeQV'
-MY_CONSUMER_SECRET = 'KGm5zr25iOfbbwlZbtUK4oEwPq9PKw7KCaQCiLF6nZyP00dskd'
-MY_ACCESS_TOKEN_KEY = '822493022860574723-DLaHanWhRyX1JxbwJZKVac45FebnqPM'
-MY_ACCESS_TOKEN_SECRET = 'dE8tLlePWmchmN2fG3KoQhuiOo3sxEfrv30hwl6P05iZv'
+MY_CONSUMER_KEY = 'SaGUBfxGjMMsvfNXJdAnXyjcG'
+MY_CONSUMER_SECRET = 'rOm99m2SjqyX68DDsiVFvrsyCYRH12BEN241ZtEdogQLsK6KQn'
+MY_ACCESS_TOKEN_KEY = '822493022860574723-v6b6glgbHV0TkxXbgkdoRDrKp3L3e2b'
+MY_ACCESS_TOKEN_SECRET = 'MoyLvHPN1Z1uGrQuN7Ctiy4I1qQwd8Nrs463iaRBjrEO7'
 
 SOURCE_ACCOUNTS = ["neymarjr"] #A list of comma-separated, quote-enclosed Twitter handles of account that you'll generate tweets based on. It should look like ["account1", "account2"]. If you want just one account, no comma needed.
 ODDS = 1 #How often do you want this to run? 1/8 times?
